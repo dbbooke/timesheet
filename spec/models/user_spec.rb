c0c00947682645bf47ae4c14b59b423345d4fec8
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2018-08-26 13:02:06 -0700

    used resource generator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe User, type: :model do
 		it "cannot be valid without first_name, last_name" do
 			@user.first_name = nil
 			@user.last_name = nil
-			expect(@user).to be_valid
+			expect(@user).to_not be_valid
 		end
 	end
 end
