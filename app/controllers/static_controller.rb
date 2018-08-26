bf4b72ae4a83e735b6a52b100158fa1657b0f4cf
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2018-08-26 00:02:14 -0700

    added rspec and capybara

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,6 @@
+class StaticController < ApplicationController
+
+	def hompage
+	end
+
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+	describe "creation" do
+		before do
+		 @user = User.create(email: "test@test.com", password: "Darren", password_confirmation: "Darren", first_name: "Me", last_name: "hello")
+		end
+
+		it "can be created" do
+			expect(@user).to be_valid
+		end
+
+		it "cannot be valid without first_name, last_name" do
+			@user.first_name = nil
+			@user.last_name = nil
+			expect(@user).to be_valid
+		end
+	end
+end
