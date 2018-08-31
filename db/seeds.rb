634f6bd461f9f06825682f0c7fa03ee728dd1a30
Darren Booker <darrenbooker@Darrens-MacBook-Pro.local>
2018-08-30 21:56:14 -0700

    updated posts view fules

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,8 @@
+@user = User.create(email: "hi@test.com", password: "Darren", password_confirmation: "Darren", first_name: "Me", last_name: "hello")
+puts "1 user created"
+
 100.times do |post|
-	Post.create(date: Date.today, rationale: "#{post} rationale content")
+	Post.create(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 posts have been created"
